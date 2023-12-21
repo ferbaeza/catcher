171792373a1a992a7a740276a10edfd691c83fa0
Fer Baeza <fbaezahurtado@gmail.com>
2023-12-21 13:03:03 +0000

    create structure

--- src/Factory/Yaml.php
+++ src/Factory/Yaml.php
@@ -10,23 +10,24 @@ class Yaml
     {
         $yaml = '';
         $indent = 0;
-        foreach ($exceptions as $key => $value) {
-            $yaml .= $key . ':' . self::$salto;
+        foreach ($exceptions as $fecha => $value) {
+            $yaml .= '- fecha: ' .$fecha . self::$salto;
             $indent++;
-            foreach ($value as $key2 => $value2){
-                $yaml .= str_repeat(' ', $indent * 2) . $key2 . ':' . self::$salto;
+            foreach ($value as $id => $value2){
+                $yaml .= str_repeat(' ', $indent * 2) . '- id: '.$id . self::$salto;
                 $indent++;
                 foreach ($value2 as $key3 => $value3){
-                    $yaml .= str_repeat(' ', $indent * 2) . $key3 . ':' . $value3 . self::$salto;
+                    $yaml .= str_repeat(' ', $indent * 3) . $key3 . ': ' . $value3 . self::$salto;
                 }
                 $indent--;
             }
         }
         $name = date('Y-m-d').'_exceptions.yaml';
-        $file = fopen($name, 'w');
+        $ruta = resources('yaml');
+        $destino = $ruta.'/'. $name;
+
+        $file = fopen($destino, 'w');
         fwrite($file, $yaml);
         fclose($file);
-        $destino = resources('yaml');
-        file_put_contents($destino.'/'.$name, $yaml);
     }
 }
